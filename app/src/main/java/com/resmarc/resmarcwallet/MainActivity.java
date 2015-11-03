677b644fd107a8d66da852e14dacc2c37c78c84b
Tharindu Imesh <punchihewaimesh@gmail.com>
2015-11-03 20:49:33 +0530

    main view and activate view
    Main view contain button to activate nfc and spinner to check balance
    and history
    Activate view show nfc is enables or not

--- app/src/main/java/com/resmarc/resmarcwallet/MainActivity.java
+++ app/src/main/java/com/resmarc/resmarcwallet/MainActivity.java
@@ -1,16 +1,64 @@
 package com.resmarc.resmarcwallet;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
-public class MainActivity extends ActionBarActivity {
+import com.resmarc.resmarcwallet.adapter.MenuAdapter;
 
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+//    ArrayAdapter<CharSequence> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.menu_spinner);
+        Button activateButton  = (Button) findViewById(R.id.activate_button);
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Balance");
+        list.add("History");
+        list.add("Option");
+        MenuAdapter adapter = new MenuAdapter(MainActivity.this, android.R.layout.simple_spinner_dropdown_item,list);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    // show balance
+                } else {
+                    // show history
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinner.setSelection(2);
+
+        activateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ActivatedActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
@@ -27,8 +75,10 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+
+//        noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             return true;
         }
 
